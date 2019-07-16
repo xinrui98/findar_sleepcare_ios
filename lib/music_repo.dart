751a7e0ce927601a2942c3a408a3fa24bc2367a3
Gao Xinrui <xinrui98@users.noreply.github.com>
2019-07-16 13:01:53 +0800

    updated sounds and categorised them into levels

--- lib/music_repo.dart
+++ lib/music_repo.dart
@@ -3,14 +3,14 @@ import 'music.dart';
 class MusicRepo {
   static String defaultText = "Default Text";
   List allMusic = [
-    Music("Dreaming", "assets/dreaming.mp3", "assets/dreaming.jpg",defaultText,1178),
-    Music("Fountain Bubbling", "assets/fountain_bubbling.mp3","assets/fountain_bubbling1.jpg",defaultText,1200),
-    Music("Harmony", "assets/harmony.mp3", "assets/harmony.jpg", defaultText,1177),
-    Music("Gentle Lake", "assets/gentle_lake.mp3", "assets/gentle_lake.jpg",defaultText,1198),
-    Music("Ocean Waves", "assets/ocean_waves.mp3", "assets/ocean_waves.jpg",defaultText,1198),
-    Music("Tranquility", "assets/tranquility.mp3", "assets/tranquility.jpg",defaultText,1176),
-    Music("Clouds", "assets/clouds_in_the_sky.mp3", "assets/clouds.jpg",defaultText,1198),
-    Music("Force Of Nature", "assets/force_of_nature.mp3", "assets/force_of_nature.jpg",defaultText,1198),
+    Music("Fountain Bubbling", "assets/l1_fountain_bubbling.mp3", "assets/fountain_bubbling1.jpg","Level 1", 1200),
+    Music("Gentle Lake", "assets/l1_gentle_lake.mp3","assets/gentle_lake.jpg","Level 1",1198),
+    Music("Ocean Waves", "assets/l1_ocean_waves.mp3", "assets/ocean_waves.jpg", "Level 1",1198),
+    Music("Clouds", "assets/l2_clouds_in_the_sky.mp3", "assets/clouds.jpg","Level 2",1198),
+    Music("Dreaming", "assets/l2_dreaming.mp3", "assets/dreaming.jpg","Level 2",1178),
+    Music("Force Of Nature", "assets/l2_force_of_nature.mp3", "assets/force_of_nature.jpg","Level 2",1198),
+    Music("Harmony", "assets/l2_harmony.mp3", "assets/harmony.jpg","Level 2",1177),
+    Music("Tranquility", "assets/l2_tranquility.mp3", "assets/tranquility.jpg","Level 2",1176),
   ];
 
   List get musicList => allMusic;
